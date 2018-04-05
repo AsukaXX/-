74b55ecfecc7ff514daef177e4427ae0accc13c7
Asuka <Asuka@ASUKA-PC-X1>
2018-04-05 16:18:07 +0800

    添加项目文件。

---
+++ ConsoleApplication1/Text.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<memory>
+#include<set>
+#include<map>
+#include<vector>
+#include<string>
+#include<fstream>
+#include<sstream>
+using namespace std;
+using line_no = vector<string>::size_type;
+
+class QueryResult;
+class TextQuery {
+public:
+	TextQuery(ifstream&);
+	QueryResult query(const string&) const;
+private:
+	shared_ptr<vector<string>> file;
+	map < string, shared_ptr<set<line_no>>> wm;
+};
+
+  TextQuery::TextQuery(ifstream& is) :file(new vector<string>) {
+	string text;
+	while (getline(is, text)) {
+		file->push_back(text);
+		int n = file->size() - 1;
+		istringstream line(text);
+		string word;
+		while (line >> word) {
+			auto &lines = wm[word];
+			if (!lines)
+				lines.reset(new set<line_no>);
+			lines->insert(n);
+		}
+	}
+}
+
+
+class QueryResult {
+	friend ostream& print(ostream&, const QueryResult&);
+public:
+	QueryResult(string s, shared_ptr<set<line_no>> p, shared_ptr<vector<string>> f) :sought(s), lines(p), file(f) {};
+private:
+	string sought;
+	shared_ptr<set<line_no>> lines;
+	shared_ptr<vector<string>> file;
+};
+
+QueryResult TextQuery::query(const string& sought) const {
+	static shared_ptr<set<line_no>> no_data(new set<line_no>);
+	auto loc = wm.find(sought);
+	if (loc == wm.end())
+		return QueryResult(sought, no_data, file);
+	else
+		return QueryResult(sought, loc->second, file);
+}
+
+string make_plural(int size, string str1, string str2) {
+	if (size > 1)
+		return str1 + str2;
+	else
+		return str1;
+}
+
+ostream& print(ostream& os, const QueryResult& qr) {
+	os << qr.sought << "occurs" << qr.lines->size() << " " << make_plural(qr.lines->size(), "time", "s") << endl;
+	for (auto num : *qr.lines)
+		os << "/t(line" << num + 1 << ") " << *(qr.file->begin() + num) << endl;
+	return os;
+}
+
+void runQueries(ifstream &infile) {
+	TextQuery tq(infile);
+	while (true) {
+		cout << "enter word to look for , or q to quit:";
+		string s;
+		if (!(cin >> s) || s == "q") break;
+		print(cout, tq.query(s)) << endl;
+	}
+}
+
+int main() {
+	ifstream fs;
+	fs >> "t1.txt";
+	runQueries(fs);
+	return 0;
+}
